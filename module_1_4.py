62cee0e66399a06a8ecfe2e4ed945d1d67d945aa
Игорь <temruk_siv@mail.ru>
2024-09-12 23:14:16 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input()
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+no_spaces = my_string.replace(" ","")
+print(no_spaces)
+print(my_string[0])
+print(my_string[-1])
